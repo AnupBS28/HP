ac3bdedc15207dfbdc88941add22873dec955830
AnupBS28 <52864920+AnupBS28@users.noreply.github.com>
2020-04-20 01:31:45 +0530

    Delete exe3.java

--- exe3.java
+++
@@ -1,9 +0,0 @@
-public class MyRunnable implements Runnable {
-public void run() {
-System.out.println("Current executing thread :"+Thread.currentThread().getName());
-}
-public static void main(String[] args) throws InterruptedException {
-Thread myThread = new Thread(new MyRunnable(), "AnupBS");
-myThread.start();
-}
-}
